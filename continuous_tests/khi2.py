86e38877f741e276dbd6ff49b3f3e5c1654ae09a
radioGiorgio <florent.delgrange@gmail.com>
2016-04-25 17:06:38 +0200

    Histogram fixed

--- continuous_tests/khi2.py
+++ continuous_tests/khi2.py
@@ -5,14 +5,13 @@ import matplotlib.pyplot as plt
 def histopi(datas):
   dataset = []
   for i in range(10):
-    dataset.append([0])
+    dataset.append(0)
   for data in datas:
     dataset[data] += 1
-  histo = plt.hist(data, bin = 10)
-  plt.clf()
+  plt.hist(datas, bins=(-0.5,0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5))
   plt.xlabel('pi digits')
   plt.ylabel('ocurence')
-  plt.axis([0, 10, 0, 20000])
+  plt.axis([-1, 10, 0, 115000])
   plt.savefig("histopi.png", bbox_inches='tight')
   return dataset
 
