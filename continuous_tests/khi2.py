852949ecab7ebd8d2bcddcbdec56d4da3660095a
radioGiorgio <florent.delgrange@gmail.com>
2016-04-26 11:32:14 +0200

    Kr function added

--- continuous_tests/khi2.py
+++ continuous_tests/khi2.py
@@ -3,6 +3,7 @@ import math
 import matplotlib.pyplot as plt
 
 def histopi(datas):
+  """Creates a discrete dataset from datas and draw the linked histogram"""
   dataset = []
   for i in range(10):
     dataset.append(0)
@@ -15,4 +16,16 @@ def histopi(datas):
   plt.savefig("histopi.png", bbox_inches='tight')
   return dataset
 
+def sumf(f, i, j):
+  """definition of the sum : ∑(i ≤ j) f(i)"""
+  def sumrec(i, acc):
+    if i > j:
+      return acc
+    else:
+      return sumrec(i + 1, acc + f(i))
+  sumrec(i, 0)
 
+def k(dataset):
+  effective = float(sum(dataset))/len(dataset)
+  f = lambda i: ((dataset[i] - effective)/math.sqrt(effective))**2
+  return sumf(f, 0, len(dataset) - 1)
