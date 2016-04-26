9857ccc110d6b0a643d75fc264cb8cdbd1a66c35
radioGiorgio <florent.delgrange@gmail.com>
2016-04-26 14:33:06 +0200

    refactoring : khi2 histogram in now linked with khi2.py

--- continuous_tests/khi2.py
+++ continuous_tests/khi2.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
 import math
+import matplotlib.pyplot as plt
+
+def histopi(datas):
+  """Creates a discrete dataset from datas and draw the linked histogram"""
+  dataset = []
+  for i in range(10):
+    dataset.append(0)
+  for data in datas:
+    dataset[data] += 1
+  plt.hist(datas, bins=(-0.5,0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5))
+  plt.xlabel('pi digits')
+  plt.ylabel('ocurence')
+  plt.axis([-1, 10, 0, 115000])
+  plt.savefig("khi2_histopi.png", bbox_inches='tight')
+  return dataset
 
 def sumf(f, i, j):
   """definition of the sum : ∑(i ≤ j) f(i)"""
