cbefae32badee2837e431d1c32cf5325e369b0e7
radioGiorgio <florent.delgrange@gmail.com>
2016-04-26 16:02:28 +0200

    khi_2 tabular generator added

--- main_tests.py
+++ main_tests.py
@@ -6,4 +6,4 @@ dataset = khi2.histopi(piLoader.piDigits())
 print dataset
 print latex.table_generator("r", dataset, 10 * [10000])
 k = khi2.k(dataset)
-print k
+print latex.khi2_table_generator(k, len(dataset))
