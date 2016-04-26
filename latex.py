c4efd1dc8c6bc441e0f5a19acd24dc46862afb8b
radioGiorgio <florent.delgrange@gmail.com>
2016-04-26 14:26:24 +0200

    table centerized

--- latex.py
+++ latex.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 def table_generator(r, observed, theorical):
-  tabular = "\\begin{tabular}{|c|c|c|}\n"
+  tabular = "\\begin{figure}[h!]\n\\begin{center}\n"
+  tabular += "\\begin{tabular}{|c|c|c|}\n"
   tabular += "\\hline\n"+ r +" & eff. observé & eff. théorique \\\\\n\\hline\n"
   for i in range(len(observed)):
     tabular += str(i+1) + " & " + str(observed[i]) + " & " + str(theorical[i])
     tabular += "\\\\\n"
-  tabular += "\hline\n\end{tabular}"
+  tabular += "\hline\n\end{tabular}\n"
+  tabular += "\\end{center}\n\\caption{Tableau des effectifs pour chaque classe.}\n\\end{figure}"
   return tabular
