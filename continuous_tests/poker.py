0f188598736646d59fd19ce0c0c763c9d3192529
Clément Tamines <clement.tamines@gmail.com>
2016-04-26 23:24:57 +0100

    More implementation of poker test (added 5 digits number generator and stirling number generator)

--- continuous_tests/poker.py
+++ continuous_tests/poker.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-def occurrence_counter(number):
+def occurrence_counter2(number):
     """Counts the number of different digits in a number"""
     occurrences = []
     for digit in str(number):
@@ -9,4 +9,38 @@ def occurrence_counter(number):
             occurrences.append(digit)
     return len(occurrences)
 
-print occurrence_counter(6239111)
+
+def occurrence_counter(number):
+    """Counts the number of different digits in a number"""
+    occurrences = []
+    for digit in number:
+        if digit not in occurrences:
+            occurrences.append(digit)
+    return len(occurrences)
+
+
+def poker_test(numbers):
+    """Transforms a list of numbers into a list of 5-digits integers"""
+    n = len(numbers)
+    classes = [0, 0, 0, 0, 0]
+
+    i = 0
+    stop = n-5
+    while i <= stop:
+        number = numbers[i:(i+5)]
+        classes[occurrence_counter(number)-1] += 1
+        i+=5
+
+    return classes
+
+
+
+
+
+def stirling(k,r):
+    if k==r or r==1:
+        return 1
+    return stirling(k-1,r-1) + r*stirling(k-1,r)
+
+test = [1,2,3,4,5,  7,7,7,7,7, 8,9,9,8,0]
+print poker_test(test)
