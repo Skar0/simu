42ccd1c67abaa1cfcecfce10e9be8349fd2e52b9
Clément Tamines <clement.tamines@gmail.com>
2016-04-27 23:41:11 +0100

    More coupon collector implementation (theoretical values and bug fix on observed values)

--- continuous_tests/coupons.py
+++ continuous_tests/coupons.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+import math
+
+from continuous_tests import poker
+from tools import piLoader
+from continuous_tests import khi2
 
 
 def count(dataset):
@@ -13,6 +18,7 @@ def count(dataset):
         if len(temp) == 10:
             temp = []
             i = len(data)
+            data = []
             try :
                 length[i]
             except IndexError:
@@ -21,5 +27,24 @@ def count(dataset):
             finally:
                 length[i] +=1
                 nbr +=1
+    return (nbr, length)
+"""seq r de long, d digits, sur x echantillons"""
+def theoretical(r, d, n):
+    probas = []
+    for i in range(n):
+        probas.append(0)
+        """prob est 0 avant 10"""
+    for j in range(10,n):
+        prob = (math.factorial(d)/math.pow(d,r))*poker.stirling(r-1,d-1)
+        probas[j] = prob
+    return map(lambda x: x*n, probas)
 
+def test():
+    pi = piLoader.piDigits()
+    effective = count(pi)
+    effectiveval = effective[1]
+    theo = theoretical(10,10,effective[0])
 
+    return khi2.k(effectiveval,theo)
+
+print test()
