981f6d15798e37c63195d9b7f497f6b6738519a4
radioGiorgio <florent.delgrange@gmail.com>
2016-04-30 17:33:09 +0200

    code refactored

--- main_tests.py
+++ main_tests.py
@@ -3,13 +3,20 @@ import latex
 from tools import piLoader
 
 datas = piLoader.piDigits()
-dataset = khi2.histopi(datas)
-print latex.table_generator("r", dataset, 10 * [10000])
-k = khi2.k(dataset)
-print latex.khi2_table_generator(k, len(dataset))
-gap.histopi(datas, 5, 10)
-dataset = gap.gap_dataset(datas, 5, 10)
-print gap.theorical_effective(dataset, 5, 10)
-print latex.table_generator("r", dataset, gap.theorical_effective(dataset, 5, 10))
-khigap = khi2.k(dataset, gap.theorical_effective(dataset, 5, 10))
-print latex.khi2_table_generator(khigap, len(dataset))
+
+def khi2_test():
+  dataset = khi2.histopi(datas)
+  print latex.table_generator("r", dataset, 10 * [10000])
+  k = khi2.k(dataset)
+  print latex.khi2_table_generator(k, len(dataset))
+
+def gap_test(digit):
+  gap.histopi(datas, digit)
+  dataset = gap.test(datas, digit)
+  theoretical_values = gap.theoretical_effective(dataset, 80)
+  print latex.table_generator("longueur du gap", dataset, theoretical_values, 0)
+  khi_gap = khi2.k(dataset, theoretical_values)
+  print latex.khi2_table_generator(khi_gap, len(dataset))
+
+khi2_test()
+gap_test(5)
