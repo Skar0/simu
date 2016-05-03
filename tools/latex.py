fc3558fa317c568c0ab3baf8d320a03616a03422
radioGiorgio <florent.delgrange@gmail.com>
2016-05-03 13:51:36 +0200

    table changed in longtable

--- tools/latex.py
+++ tools/latex.py
@@ -2,20 +2,20 @@
 from scipy.stats import chi2
 
 def table_generator(r, observed, theoretical, beginning=1):
-  tabular = "\\begin{figure}[h!]\n\\begin{center}\n"
-  tabular += "\\begin{tabular}{|c|c|c|}\n"
+  tabular = "\\begin{figure}[H]\n\\begin{center}\n"
+  tabular += "\\begin{longtable}{|c|c|c|}\n"
   tabular += "\\hline\n"+ r +" & eff. observé & eff. théorique \\\\\n\\hline\n"
   for i in range(len(observed)):
     n = i + beginning
     tabular += str(n) + " & " + str(observed[i]) + " & " + str(theoretical[i])
     tabular += "\\\\\n"
-  tabular += "\hline\n\end{tabular}\n"
+  tabular += "\hline\n\end{longtable}\n"
   tabular += "\\end{center}\n\\caption{Tableau des effectifs pour chaque classe.}\n\\end{figure}"
   return tabular
 
 def khi2_table_generator(K, n):
   alpha = [0.001, 0.025, 0.05, 0.1]
-  tabular = "\\begin{figure}[h!]\n\\begin{center}\n"
+  tabular = "\\begin{figure}[H]\n\\begin{center}\n"
   tabular += "\\begin{tabular}{|c|c|c|c|}\n"
   tabular += "\\hline\n$\\alpha$ & $K_n$ & $\\chi^2$ & Résultat\\\\\n\\hline\n"
   for a in alpha:
