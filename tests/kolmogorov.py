1988451c026726b3d31214d21caafcd4581882fc
Clément Tamines <clement.tamines@gmail.com>
2016-06-01 17:46:17 +0200

    Moved pirand result test file. Commented kolmogorov

--- tests/kolmogorov.py
+++ tests/kolmogorov.py
@@ -3,7 +3,9 @@ from tools import piLoader as loader
 
 
 def kolmo(data, theoretical_distribution):
+    """Computes the maximum gap between the theoretical distribution given as argument and the empirical distribution"""
     n = len(data)
+    # Sorting data first
     sorted_data = sorted(data)
     empirical = []
     cumulative = []
@@ -15,11 +17,15 @@ def kolmo(data, theoretical_distribution):
         cumulative.append(theoretical_distribution(number))
         gaps.append(abs(empirical[j]-cumulative[j]))
         j+=1
+
+    # Maximum gap
     max_gap = max(gaps)
     return max_gap
 
 
 def empirical_distribution(data, x, n):
+    """Returns the number of values that are smaller than x in the array data and divides it by the total number of
+    numbers in data"""
     i = 0
     while i < n and data[i] <= x:
       i+=1
@@ -27,6 +33,7 @@ def empirical_distribution(data, x, n):
 
 
 def theoretical_distribution(x):
+    """Theoretical distribution here is the continuous uniform distribution between 0 and 1"""
     a = 0
     b = 1
     if x < a:
